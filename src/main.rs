180e7cfc329402af71edeb9d0d983e9ebac8e0b6
michbogos <bogostamicheal@gmail.com>
2024-10-17 09:49:45 +0200

    Add panic messages

--- src/main.rs
+++ src/main.rs
@@ -10,14 +10,13 @@ mod vga;
 /// This function is called on panic.
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
+    write!(*vga::VGA.lock(), "{}", _info.message()).unwrap();
     loop {}
 }
 
-static HELLO: &[u8] = b"";
-
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    write!(*vga::VGA.lock(), "Hello {}\n", 1.2);
+    write!(*vga::VGA.lock(), "Hello {}\n", 1.2).unwrap();
     // *vga_buffer.offset(i as isize * 2) = byte;
     // *vga_buffer.offset(i as isize * 2 + 1) = i as u8;
 
